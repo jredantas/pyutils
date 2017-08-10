429c16060c131254c78a04d2d2b3e48696f60969
jredantas <jrdantas@yahoo.com.br>
2017-08-10 15:58:59 -0300

    Correct setup.py.

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ Created on Thu Aug 10 15:28:50 2017
 
 from setuptools import setup
 
-setup(name='utils',
+setup(name='pyutils',
       version='0.1',
       description='Utilities for Python projects',
-      url='http://github.com/storborg/funniest',
+      url='http://github.com/jredantas/pyutils',
       author='Jose Renato Dantas',
       author_email='jrdantas@yahoo.com.br',
       license='creative commons',
-      packages=['utils'],
+      packages=['pyutils'],
       zip_safe=False)
