e291f2b5232433a27e6349a2094fdbfe213f73aa
jredantas <jrdantas@yahoo.com.br>
2017-08-10 15:38:15 -0300

    Adding utils

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 15:28:50 2017
+
+@author: 09959295800
+"""
+
+from setuptools import setup
+
+setup(name='utils',
+      version='0.1',
+      description='Utilities for Python projects',
+      url='http://github.com/storborg/funniest',
+      author='Jose Renato Dantas',
+      author_email='jrdantas@yahoo.com.br',
+      license='creative commons',
+      packages=['utils'],
+      zip_safe=False)
